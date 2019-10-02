3ffc39e3bd2c5857d774b9db810efe6b013f6e8d
Israel Rojas <israelrojasshema@gmail.com>
2019-10-02 11:20:26 -0500

    Update

--- sitecode/logic.js
+++ sitecode/logic.js
@@ -2,8 +2,8 @@ $("#about").click(function() {
   window.scrollTo({ top: 0, behavior: "smooth" });
 });
 $("#portfolio").click(function() {
-  window.scrollTo({ top: 790, behavior: "smooth" });
+  window.scrollTo({ top: 660, behavior: "smooth" });
 });
 $("#contacts").click(function() {
-  window.scrollTo({ top: 1530, behavior: "smooth" });
+  window.scrollTo({ top: 1375, behavior: "smooth" });
 });
